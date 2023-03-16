653fb6781cdf206273aee20ec2ac78c1e31694c6
Pope <andreipope16@gmail.com>
2023-03-16 02:08:58 +0200

    done

--- src/lib/axiosRequest.js
+++ src/lib/axiosRequest.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const baseURL = "http://localhost:5000/api";
+// const baseURL = "http://localhost:5000/api";
+const baseURL = "https://social-media-backend-cthx.onrender.com/api";
 export const createAxiosInstance = (token) => {
   const instance = axios.create({
     baseURL,
